43ae040273875ac0fa303ea39666d25e39980e0e
Arthur Makaev <unium.orb.1@gmail.com>
2021-02-25 19:05:03 +0500

    test

--- index.php
+++ index.php
@@ -72,54 +72,6 @@ function send($path, $chatid, $message, $buttons){
 
 
 ?>
-<!-- <?php
-//https://api.telegram.org/bot1387467862:AAHm63q6W4xRwnO17ddPhV_uxIZAZncOcww/setwebhook?url=https://amarthurabot.000webhostapp.com/amarthurabot.php
-$token="1387467862:AAHm63q6W4xRwnO17ddPhV_uxIZAZncOcww";
-$path="https://api.telegram.org/bot1387467862:AAHm63q6W4xRwnO17ddPhV_uxIZAZncOcww";
-//$token="1387467862:AAHm63q6W4xRwnO17ddPhV_uxIZAZncOcww";
-//$path="https://api.telegram.org/bot1387467862:AAHm63q6W4xRwnO17ddPhV_uxIZAZncOcww";
-$update = json_decode(file_get_contents("php://input"), TRUE);
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
-
-
-$api = 'https://api.telegram.org/bot' . $token;
-
-
-$callback_query = $update['callback_query'];
-$data = $callback_query['data'];
-$message_id = ['callback_query']['message']['message_id'];
-$chat_id_in = $callback_query['message']['chat']['id'];
-
-if (strpos($message, "/info") === 0) {
-   
-    
-    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather u ");
-}
-
-
-if (strpos($message, "/text") === 0){
-  
-        $inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
-        $inline_button2 = array("text"=>"work plz","callback_data"=>'/plz');
-        $inline_keyboard = [[$inline_button1,$inline_button2]];
-        $keyboard=array("inline_keyboard"=>$inline_keyboard);
-        $replyMarkup = json_encode($keyboard); 
-        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather pu ".'&text='."key".'&reply_markup=' . $replyMarkup);
-        
-}
-switch($data){
-    case '/plz':
-        $inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
-        $inline_button2 = array("text"=>"work plz","callback_data"=>'/plz');
-        $inline_keyboard = [[$inline_button1,$inline_button2]];
-        $keyboard=array("inline_keyboard"=>$inline_keyboard);
-        $replyMarkup = json_encode($keyboard); 
-        file_get_contents($path."/sendmessage?chat_id=".$chat_id_in."&text=Here's the weather pu ".'&text='."key".'&reply_markup=' . $replyMarkup);
-    break;
-}
-
-?> -->
 
 
 
